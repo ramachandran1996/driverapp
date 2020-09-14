2fe76b83ef36568fe5a568825c3e68700adc3274
YouCorp-2 <ramachandran.191996@gmail.com>
2020-09-14 22:02:51 +0530

    darkthemes_asyncstorage

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useCallback } from "react";
 import { View, Button, Text } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import {
@@ -7,13 +7,15 @@ import {
   DefaultTheme as NavigationDefaultTheme,
   DarkTheme as NavigationDarkTheme,
 } from "@react-navigation/native";
+import AsyncStorage from "@react-native-community/async-storage";
 
 import { DrawerContent } from "./src/Router/drawercontent";
 import { Maintab } from "./src/Router/index";
 import { AuthContext } from "./src/Router/context";
-
+let variable = "";
 export default function App() {
   const [isDarkTheme, setIsDarkTheme] = React.useState(false);
+  const [isloading, setIsloading] = React.useState(false);
   const Drawer = createDrawerNavigator();
   const CustomDefaultTheme = {
     ...NavigationDefaultTheme,
@@ -32,26 +34,50 @@ export default function App() {
       text: "#ffffff",
     },
   };
-  const authContext = React.useMemo(
-    () => ({
-      toggleTheme: () => {
-        setIsDarkTheme((isDarkTheme) => !isDarkTheme);
-      },
-    }),
-    []
-  );
-  const theme = isDarkTheme ? CustomDarkTheme : CustomDefaultTheme; 
-  return (
-    <AuthContext.Provider value={authContext}>
-      <NavigationContainer theme={theme}>
-        <Drawer.Navigator
-          drawerContent={(props) => <DrawerContent {...props} />}
-        >
-          <Drawer.Screen name="Home" component={Maintab} />
-          {/* <Drawer.Screen name="Notifications" component={ProfileStackScreen} /> */}
-        </Drawer.Navigator>
-      </NavigationContainer>
-    </AuthContext.Provider>
+  // const authContext = React.useMemo(
+  //   () => ({
+  //     toggleTheme: () => {
+  //       setIsDarkTheme((isDarkTheme) => !isDarkTheme);
+  //     },
+  //   }),
+  //   []
+  // );
+  useEffect(() => {
+    getData();
+    console.log("useseffesct");
+  }, [isDarkTheme]);
+
+  const getData = useCallback(async () => {
+    setIsloading(false);
+    try {
+      const value = await AsyncStorage.getItem("@themeswitch");
+      console.log("ram", value);
+      if (value != null) {
+        if (value == "true") {
+          setIsDarkTheme(true);
+          variable = value;
+        } else {
+          setIsDarkTheme(false);
+          variable = value;
+        }
+        setIsloading(true);
+      }
+    } catch (e) {
+      // error reading value
+    }
+  }, []);
+  const theme = isDarkTheme ? CustomDarkTheme : CustomDefaultTheme;
+
+  return isloading ? (
+    // <AuthContext.Provider value={authContext}>
+    <NavigationContainer theme={theme}>
+      <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
+        <Drawer.Screen name="Home" component={Maintab} />
+        {/* <Drawer.Screen name="Notifications" component={ProfileStackScreen} /> */}
+      </Drawer.Navigator>
+    </NavigationContainer>
+  ) : (
+    <Text>loading</Text>
   );
 }
 
--- src/Router/drawercontent.tsx
+++ src/Router/drawercontent.tsx
@@ -1,26 +1,47 @@
-import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import {
-  View,
-  Text,
-  StyleSheet,
-  SafeAreaView,
-  Switch,
-  TouchableOpacity,
-} from "react-native";
+import React, { useEffect, useState, useCallback } from "react";
+// import { StatusBar } from "expo-status-bar";
+import { View, Text, Switch, TouchableOpacity } from "react-native";
 import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
 import { useTheme } from "@react-navigation/native";
-import { color } from "react-native-reanimated";
+// import { color, Value } from "react-native-reanimated";
+import AsyncStorage from "@react-native-community/async-storage";
 
-import { AuthContext } from "./context";
-
+// import { AuthContext } from "./context";
+let storetostring = "";
 export function DrawerContent(props) {
   const [isEnabled, setIsEnabled] = useState(false);
+  const [isRender, setIsrender] = useState(false);
   const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
   const { colors } = useTheme();
   const paperTheme = useTheme();
-  const { toggleTheme } = React.useContext(AuthContext);
+  // const { toggleTheme } = React.useContext(AuthContext);
+  const togglestorage = async (value) => {
+    try {
+      setIsEnabled((previousState) => !previousState);
+      console.log("1enabled", isEnabled, value);
+      // storetostring = isEnabled ? "true" : "false";
+      storetostring = value ? "true" : "false";
+      await AsyncStorage.setItem("@themeswitch", storetostring);
+      // console.log("1enabledrender", storetostring);
+    } catch (e) {
+      // saving error
+      console.log("error", e);
+    }
+  };
+  const theme = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@themeswitch");
 
+      setIsEnabled(value == "true" ? true : false);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    theme();
+  }, [isEnabled]);
+  // console.log("2enabledrender", isEnabled);
   return (
     <View style={{ flex: 1, backgroundColor: colors.background }}>
       <DrawerContentScrollView {...props}>
@@ -31,10 +52,8 @@ export function DrawerContent(props) {
               trackColor={{ false: "#767577", true: "#81b0ff" }}
               thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
               ios_backgroundColor="#3e3e3e"
-              onValueChange={() => {
-                toggleTheme();
-              }}
-              value={paperTheme.dark}
+              onValueChange={(value) => togglestorage(value)}
+              value={isEnabled}
             />
             {/* <TouchableOpacity
               onPress={() => {
